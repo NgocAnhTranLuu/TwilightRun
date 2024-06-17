e583d54005a9cf24a5eae34c8cf48b34b48d613d
Shyn <tqforwork2@gmail.com>
2024-06-17 22:17:23 +0700

    toi 17/6

--- JS/Register.js
+++ JS/Register.js
@@ -1,70 +1,68 @@
-
 // ĐỔI MÀU KHI CLICK Ở TRANG MUA BIB
 
 // Thẻ chọn cự ly
-let SSkmItem = document.querySelectorAll('.SSkmItem');
+let SSkmItem = document.querySelectorAll(".SSkmItem");
 
-SSkmItem.forEach(item => {
-    item.addEventListener('click', function() {
+SSkmItem.forEach((item) => {
+  item.addEventListener("click", function () {
     // Xóa class "SSkmActive" khỏi tất cả các thẻ SSkmItem
-    SSkmItem.forEach(otherItem => {
-      otherItem.classList.remove('SSkmActive');
+    SSkmItem.forEach((otherItem) => {
+      otherItem.classList.remove("SSkmActive");
     });
 
     // Thêm class "SSkmActive" cho thẻ SSkmItem được click
-    this.classList.add('SSkmActive');
+    this.classList.add("SSkmActive");
   });
 });
 
-
 // Thẻ chọn giới tính
-let GioiTinhItem = document.querySelectorAll('.GioiTinhItem');
+let GioiTinhItem = document.querySelectorAll(".GioiTinhItem");
 
-GioiTinhItem.forEach(item => {
-    item.addEventListener('click', function() {
-    GioiTinhItem.forEach(otherItem => {
-      otherItem.classList.remove('GioiTinhActive');
+GioiTinhItem.forEach((item) => {
+  item.addEventListener("click", function () {
+    GioiTinhItem.forEach((otherItem) => {
+      otherItem.classList.remove("GioiTinhActive");
     });
 
-    this.classList.add('GioiTinhActive');
+    this.classList.add("GioiTinhActive");
   });
 });
 
 // Thẻ chọn Nhóm máu
-let NhomMauItem = document.querySelectorAll('.NhomMauItem');
+let NhomMauItem = document.querySelectorAll(".NhomMauItem");
 
-NhomMauItem.forEach(item => {
-    item.addEventListener('click', function() {
-    NhomMauItem.forEach(otherItem => {
-      otherItem.classList.remove('NhomMauActive');
+NhomMauItem.forEach((item) => {
+  item.addEventListener("click", function () {
+    NhomMauItem.forEach((otherItem) => {
+      otherItem.classList.remove("NhomMauActive");
     });
 
-    this.classList.add('NhomMauActive');
+    this.classList.add("NhomMauActive");
   });
 });
 
 // Thẻ chọn Loại áo
-let LoaiAoItem = document.querySelectorAll('.LoaiAoItem');
+let LoaiAoItem = document.querySelectorAll(".LoaiAoItem");
 
-LoaiAoItem.forEach(item => {
-    item.addEventListener('click', function() {
-    LoaiAoItem.forEach(otherItem => {
-      otherItem.classList.remove('LoaiAoActive');
+LoaiAoItem.forEach((item) => {
+  item.addEventListener("click", function () {
+    LoaiAoItem.forEach((otherItem) => {
+      otherItem.classList.remove("LoaiAoActive");
     });
 
-    this.classList.add('LoaiAoActive');
+    this.classList.add("LoaiAoActive");
   });
 });
 
 // Thẻ chọn Size áo
-let SizeAoItem = document.querySelectorAll('.SizeAoItem');
+let SizeAoItem = document.querySelectorAll(".SizeAoItem");
 
-SizeAoItem.forEach(item => {
-    item.addEventListener('click', function() {
-    SizeAoItem.forEach(otherItem => {
-      otherItem.classList.remove('SizeAoActive');
+SizeAoItem.forEach((item) => {
+  item.addEventListener("click", function () {
+    SizeAoItem.forEach((otherItem) => {
+      otherItem.classList.remove("SizeAoActive");
     });
 
-    this.classList.add('SizeAoActive');
+    this.classList.add("SizeAoActive");
   });
 });
